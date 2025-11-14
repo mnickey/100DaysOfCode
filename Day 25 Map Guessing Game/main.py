c56f0c77d82cb22345cef30de6cb11b527a904ce
Michael Nickey <mnickey@gmail.com>
2025-11-14 06:39:52 -0800

    Create main.py

---
+++ Day 25 Map Guessing Game/main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 States",
+        prompt="Enter a state: ").title()
+    if answer_state == "Exit":
+        states_to_learn = []
+        for state in all_states:
+            if state not in guessed_states:
+                states_to_learn.append(state)
+        new_data = pandas.DataFrame(states_to_learn)
+        new_data.to_csv("states_to_learn.csv", index=False)
+        break
+
+    if answer_state in all_states and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
+turtle.mainloop()
