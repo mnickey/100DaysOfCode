fe6f61dc7f219b7dfa83f16e5b380771d1a72d9f
Michael Nickey <mnickey@gmail.com>
2025-11-20 10:26:50 -0800

    Create miles_to_kilo_converter.py

---
+++ Day 28 TKInter - Args - Kwargs/miles_to_kilo_converter.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.60934
+    kilometer_result_label.configure(text=f"{km}")
+
+window = Tk()
+window.title("Miles to KM Converter")
+window.configure(padx=20, pady=20)
+
+miles_input = Entry(width=7)
+miles_input.grid(row=0, column=1)
+
+miles_label = Label(text="Miles")
+miles_label.grid(row=0, column=2)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(row=1, column=0)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(row=1, column=1)
+
+kilometer_label = Label(text="Km")
+kilometer_label.grid(row=1, column=2)
+
+calculate_button = Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(row=2, column=1)
+
+window.mainloop()
