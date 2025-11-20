43e1a9a713eba6e45586f6c4c0616aceaf6eb787
Michael Nickey <mnickey@gmail.com>
2025-11-20 07:33:58 -0800

    Update main.py
    Added isalpha() to the list comprehension, so the result list contains only alphabetical characters.

--- Day 26 - List, Dict and Dataframe Comprehensions/main.py
+++ Day 26 - List, Dict and Dataframe Comprehensions/main.py
@@ -11,7 +11,7 @@ my_dict = {row.letter:row.code for (index, row) in alpha_dataframe.iterrows()}
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 user_input = input("Please enter your name: ").upper()
 
-result = [my_dict[letter] for letter in user_input]
+result = [my_dict[letter] for letter in user_input if letter.isalpha()]
 print(result)
 #
 # for letter in user_input:
