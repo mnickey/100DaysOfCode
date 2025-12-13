12717883bab2f3463e0521c91ae91f3c0d219466
Michael Nickey <mnickey@gmail.com>
2025-12-13 12:46:06 -0800

    Update main.py

--- Day 38  Workout Tracker w/Google Sheets & API calls/main.py
+++ Day 38  Workout Tracker w/Google Sheets & API calls/main.py
@@ -1,24 +1,31 @@
 import requests
+from datetime import datetime
+import os
+import pprint
 
 # CONSTANTS
 GENDER = "male"
 WEIGHT_KG = 205
 HEIGHT_CM = 193
 AGE = 52
-APP_ID = "YOUR_APP_ID"
-API_KEY = "YOUR_API_KEY"
-BASE_URL = "https://app.100daysofpython.dev"
-POST_URL = BASE_URL + "/v1/nutrition/natural/exercise"
 
-print(POST_URL)
-workout_data = input("Tell me what exercises you did?")
+# ENVIRONMENTAL_VARIABLES
+NUTRITION_APP_ID = os.environ["NUTRITION_APP_ID"]
+NUTRITION_API_KEY = os.environ["NUTRITION_API_KEY"]
+SHEET_ENDPOINT = os.environ["SHEET_ENDPOINT"]
+USERNAME = os.environ["USERNAME"]
 
-HEADERS = {
-    "x-app-id": APP_ID,
-    "x-app-key": API_KEY,
+NUTRITION_BASE_URL = "https://app.100daysofpython.dev"
+NUTRITION_POST_URL = NUTRITION_BASE_URL + "/v1/nutrition/natural/exercise"
+
+workout_data = input("Tell me what exercises you did? ")
+
+NUTRITION_HEADERS = {
+    "x-app-id": NUTRITION_APP_ID,
+    "x-app-key": NUTRITION_API_KEY,
 }
 
-PAYLOAD = {
+NUTRITION_PAYLOAD = {
     "query": workout_data,
     "weight_kg": WEIGHT_KG,
     "height_cm": HEIGHT_CM,
@@ -26,6 +33,42 @@ PAYLOAD = {
     "gender": GENDER,
 }
 
-response = requests.post(POST_URL, json=PAYLOAD, headers=HEADERS)
-response.raise_for_status()
-print(response.text)
+nutrition_api_response = requests.post(NUTRITION_POST_URL, json=NUTRITION_PAYLOAD, headers=NUTRITION_HEADERS)
+nutrition_api_response.raise_for_status()
+nutrition_data = nutrition_api_response.json()
+# pprint.pprint(nutrition_data)
+
+exercise_type = nutrition_data["exercises"][0]['name'].title()
+duration = nutrition_data["exercises"][0]['duration_min']
+calories = nutrition_data["exercises"][0]['nf_calories']
+
+SHEETY_BASE_URL = SHEET_ENDPOINT
+print(str(SHEETY_BASE_URL))
+
+today_date = datetime.today().strftime("%d/%m/%Y")
+current_time = datetime.now().strftime("%H:%M:%S")
+# print(today_date, current_time)
+
+SHEET_INPUTS = {
+        "workout": {
+            "date": today_date,
+            "time": current_time,
+            "exercise": exercise_type,
+            "duration": duration,
+            "calories": calories
+        }
+    }
+
+SHEETY_AUTH_TOKEN = {
+    "Authorization": os.getenv("SHEETY_AUTH_TOKEN"),
+}
+
+# GET REQUEST TO ENSURE API CALLS WORK AND SHEETY IS CONNECTED
+# response = requests.get(SHEETY_BASE_URL, params=SHEETY_PAYLOAD)
+# response.raise_for_status()
+# print(response.text)
+
+# POST REQUESTS
+sheety_post_response = requests.post(SHEETY_BASE_URL, json=SHEET_INPUTS, headers=SHEETY_AUTH_TOKEN)
+sheety_post_response.raise_for_status()
+print(sheety_post_response.text)
