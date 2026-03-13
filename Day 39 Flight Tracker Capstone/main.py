711cdf6392c0e0241bacbb143cc2f932693593bc
Michael Nickey <mnickey@gmail.com>
2026-03-12 22:32:58 -0700

    Update main.py

--- Day 39 Flight Tracker Capstone/main.py
+++ Day 39 Flight Tracker Capstone/main.py
@@ -2,18 +2,22 @@
 # to achieve the program requirements.
 import pprint
 from datetime import datetime
-import os
 from data_manager import DataManager
 from flight_search import FlightSearch
 
 # GET REQUEST TO ENSURE API CALLS WORK AND SHEETY IS CONNECTED
-data = DataManager()
+data_manager = DataManager()
 flight_search = FlightSearch()
 
-response = data.read_data()
+response = data_manager.read_data()
 sheet_data = response["prices"]
-for data in sheet_data:
-    if data['iataCode'] == '':
-        # print(data['id'])
-        data['iataCode'] = flight_search.get_iata_code()
+
+for x in range(len(sheet_data)):
+    if sheet_data[x]['iataCode'] == '':
+        city = sheet_data[x]['city']
+        id = sheet_data[x]['id']
+        price = sheet_data[x]['lowestPrice']
+        iata_code = flight_search.get_iata_code()
+        data_manager.write_data(city, iata_code, id, price)
+
 pprint.pprint(sheet_data)
