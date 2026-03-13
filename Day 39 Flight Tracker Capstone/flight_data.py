aba9ccf39938895628aba4b0505fce175425f394
Michael Nickey <mnickey@gmail.com>
2026-03-12 22:34:22 -0700

    Update flight_data.py

--- Day 39 Flight Tracker Capstone/flight_data.py
+++ Day 39 Flight Tracker Capstone/flight_data.py
@@ -1,3 +1,13 @@
-class FlightData:
-    # This class is responsible for structuring the flight data.
-    pass
+Amedeaus_API_Key = "GET YOUR OWN KEY"
+Amedeaus_Secret = "GET YOUR OWN KEY"
+
+class FlightSearch:
+    # This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        # _api_key = self.api_key
+        # _api_secret = self.api_secret
+        # _token = self.token
+        pass
+
+    def get_iata_code(self):
+        return "TESTING"
