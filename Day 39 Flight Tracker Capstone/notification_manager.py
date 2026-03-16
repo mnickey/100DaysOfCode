b354b76faba4bb91d9611065dff35b9002c0d691
Michael Nickey <mnickey@gmail.com>
2026-03-16 11:04:19 -0700

    Update notification_manager.py

--- Day 39 Flight Tracker Capstone/notification_manager.py
+++ Day 39 Flight Tracker Capstone/notification_manager.py
@@ -1,3 +1,23 @@
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# This class is responsible for sending notifications with the deal flight details.
 class NotificationManager:
-    # This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.email = os.getenv('TESTING_EMAIL')
+        self.APP_PASSWORD = os.getenv('APP_PASSWORD')
+
+    def send_email(self, message_body):
+        with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+            connection.starttls()
+            connection.login(user=self.email, password=self.APP_PASSWORD)
+            email_message = f"Subject:New Low Flight Price Found!\n\n{message_body}"
+
+            connection.sendmail(
+                from_addr=self.email,
+                to_addrs=self.email,
+                msg=email_message.encode('utf-8')
+            )
